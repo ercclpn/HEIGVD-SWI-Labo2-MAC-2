b1bf07365247df0716bbb66e11947faabdf95e46
ercclpn <ercclpn@gmail.com>
2020-03-16 16:20:05 +0100

    Temp test file.

--- script_2_1.py
+++ script_2_1.py
@@ -13,23 +13,52 @@ args = parser.parse_args()
 global staDic
 staDic = {}
 
-def sniffHandler(trame):
+global bssidLinkSta
+bssidLinkSta = {}
+
+
+def sniffProbeReqHandler(trame):
     if trame.haslayer(Dot11ProbeReq):
         if trame.haslayer(Dot11Elt):
+            if not trame.addr1 == 'ff:ff:ff:ff:ff:ff':
+                if trame.addr1 in bssidLinkSta:
+                    bssidLinkSta[trame.addr1].add(trame.addr2)
+                else:
+                    bssidLinkSta[trame.addr1] = set([trame.addr2])
+            else:
+                return
             decodeInfo = trame.info.decode("utf-8")
             if not decodeInfo : #Empty String
-                return
-            if decodeInfo in staDic:
-                staDic[decodeInfo].add(trame.payload.addr2)
-            else:
-                staDic[decodeInfo] = set([trame.payload.addr2]) #[] dans le set permet d'initialiser avec une valeur sans qu'il le casse en plusieurs caracteres.
+                if decodeInfo in staDic:
+                    staDic[decodeInfo].add(trame.addr2)
+                else:
+                    staDic[decodeInfo] = set([trame.addr2]) #[] dans le set permet d'initialiser avec une valeur sans qu'il le casse en plusieurs caracteres.
+
+def sniffAPHandler(trame):
+    if trame.haslayer(Dot11Beacon):
+        if trame.haslayer(Dot11Elt):
+            decodeInfo = trame.info.decode("utf-8")
+            if decodeInfo in staDic and not trame.addr2 in bssidLinkSta :
+               bssidLinkSta[trame.addr2] = staDic[decodeInfo]
 
 
-sniff(iface='wlo1mon', prn=sniffHandler, timeout=10) #timeout en secondes
+
+sniff(iface='wlo1mon', prn=sniffProbeReqHandler, timeout=30) #timeout en secondes
 
 for key in staDic :
     result = str(key) + " : "
     for val in staDic[key]:
         result += val + ", "
     print(result)
+
+sniff(iface='wlo1mon', prn=sniffAPHandler, timeout=30)
+
+
+for key in bssidLinkSta:
+    print(key + "   " + bssidLinkSta[key])
+
+
+
+
+
 
